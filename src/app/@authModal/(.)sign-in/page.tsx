452347c8eadc250b1e6c6b43e758c2cbc77eae8e
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-01 21:55:37 +0200

    fix

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -1,18 +1,23 @@
+"use client";
+
 import CloseModal from "@/components/CloseModal";
 import SignIn from "@/components/SignIn";
-import React, { FC } from "react";
+import { useRouter } from "next/navigation";
 
-interface pageProps {}
+const page = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const router = useRouter();
 
-const page: FC<pageProps> = () => {
     return (
-        <div className="fixed inset-0 bg-zinc-900/20 z-10">
+        <div
+            className="fixed inset-0 bg-zinc-900/20 z-10"
+            onClick={() => router.back()}
+        >
             <div className="container flex items-center h-full max-w-lg mx-auto">
                 <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
                     <div className="absolute top-4 right-4">
                         <CloseModal />
                     </div>
-
                     <SignIn />
                 </div>
             </div>
