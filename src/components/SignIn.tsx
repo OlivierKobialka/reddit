23464fc5fdd597941a69a0eef6614503932e11bf
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-01 16:47:45 +0200

    fix

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -11,13 +11,13 @@ const SignIn = () => {
                     Welcome back
                 </h1>
                 <p className="text-sm max-w-xs mx-auto">
-                    By continuing, you are setting up a Techni Reddit account and
-                    agree to our User Agreement and Privacy Policy.
+                    By continuing, you are setting up a Techni Reddit account
+                    and agree to our User Agreement and Privacy Policy.
                 </p>
             </div>
             {/* <UserAuthForm /> */}
             <p className="px-8 text-center text-sm text-muted-foreground">
-                New to Techni Reddit?
+                New to Techni Reddit?{" "}
                 <Link
                     href="/sign-up"
                     className="hover:text-brand text-sm underline underline-offset-4"
