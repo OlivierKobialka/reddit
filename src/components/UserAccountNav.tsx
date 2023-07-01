8187a57ab25c738c938d638bd1d603fa5dac1e71
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-01 21:27:12 +0200

    options

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -21,15 +21,52 @@ interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
     return (
+        // User account dropdown menu
         <DropdownMenu>
+            {/* User img badge */}
             <DropdownMenuTrigger>
                 <UserAvatar
                     user={{
                         name: user.name || null,
                         image: user.image || null,
                     }}
+                    className="h-8 w-8"
                 />
             </DropdownMenuTrigger>
+            {/* User informations: name + email */}
+            <DropdownMenuContent className="bg-white" align="end">
+                <div className="flex items-center justify-start gap-2 p-2">
+                    <div className="flex flex-col space-y-1 leading-none">
+                        {user.name && (
+                            <p className="font-medium">{user.name}</p>
+                        )}
+                        {user.email && (
+                            <p className="w-[200px] truncate text-sm text-zinc-700">
+                                {user.email}
+                            </p>
+                        )}
+                    </div>
+                </div>
+
+                <DropdownMenuSeparator />
+
+                {/* User account dropdown menu items */}
+                <DropdownMenuItem asChild>
+                    <Link href="/">Feed</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                    <Link href="/r/create">Create community</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                    <Link href="/settings">Settings</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem className="cursor-pointer">
+                    Sign out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     );
 };
