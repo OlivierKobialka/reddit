08f6cf27448068cb805e8830c606c7e0a6dac7be
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-01 21:48:22 +0200

    styling + import

---
+++ src/components/CloseModal.tsx
@@ -0,0 +1,16 @@
+import { X } from "lucide-react";
+import { Button } from "./ui/Button";
+
+const CloseModal = () => {
+    return (
+        <Button
+            variant={"subtle"}
+            className="h-6 w-6 p-0 rounded-md"
+            aria-label="Close Modal"
+        >
+            <X className="h-4 w-4" />
+        </Button>
+    );
+};
+
+export default CloseModal;
