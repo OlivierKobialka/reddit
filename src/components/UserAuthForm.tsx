ee3fbeabc3bf6a1e57b74d3e174a3b5367785595
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-01 19:11:16 +0200

    toast

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,24 +1,30 @@
 "use client";
 
-import React, { FC } from "react";
-import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
 import { signIn } from "next-auth/react";
+import * as React from "react";
+import { FC } from "react";
+import { Button } from "@/components/ui/Button";
+import { useToast } from "@/hooks/use-toast";
 import { Icons } from "./Icons";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+    const { toast } = useToast();
     const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-    const logInWithGoogle = async () => {
+    const loginWithGoogle = async () => {
         setIsLoading(true);
 
         try {
-            await signIn("google", {});
+            await signIn("google");
         } catch (error) {
-            // toast notification
-            
+            toast({
+                title: "Problem while logging!",
+                description: "There was an problem logging in with Google",
+                variant: "destructive",
+            });
         } finally {
             setIsLoading(false);
         }
@@ -27,12 +33,14 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
     return (
         <div className={cn("flex justify-center", className)} {...props}>
             <Button
+                isLoading={isLoading}
+                type="button"
                 size="sm"
                 className="w-full"
-                onClick={logInWithGoogle}
-                isLoading={isLoading}
+                onClick={loginWithGoogle}
+                disabled={isLoading}
             >
-                {isLoading ? null : <Icons.google className="mr-2 h-4 w-4" />}
+                {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
                 Google
             </Button>
         </div>
