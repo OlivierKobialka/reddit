4a73eb80e632534091c72dffdeb7b7afe0f7330f
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-03 12:26:09 +0200

    a

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -54,7 +54,7 @@ const Page = () => {
                 variant: "destructive",
             });
         },
-        
+
         onSuccess: (data) => {
             router.push(`/r/${data}`);
         },
