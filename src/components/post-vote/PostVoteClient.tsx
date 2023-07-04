b010d10242292190eed7c4645f10438b7363d704
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-04 14:10:33 +0200

    dasdas

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -8,6 +8,8 @@ import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 import { FC, useEffect, useState } from "react";
 import { Button } from "../ui/Button";
 import { useMutation } from "@tanstack/react-query";
+import { PostVoteRequest } from "@/lib/validators/vote";
+import axios from "axios";
 
 interface PostVoteClientProps {
     postId: string;
@@ -30,12 +32,15 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
     }, [currentVote]);
 
     const {} = useMutation({
-        mutationFn: async(type: VoteType) => {
-            const payload = {
-                
-            }
+        mutationFn: async (voteType: VoteType) => {
+            const payload: PostVoteRequest = {
+                postId,
+                voteType,
+            };
+
+            await axios.patch("/api/subreddit/post/vote", payload);
         },
-    })
+    });
 
     return (
         <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
@@ -54,8 +59,7 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
             <Button size="sm" variant="ghost" aria-label="downvote">
                 <ArrowBigDown
                     className={cn("h-5 w-5 text-zinc-700", {
-                        "text-red-500 fill-red-500":
-                            currentVote === "DOWN",
+                        "text-red-500 fill-red-500": currentVote === "DOWN",
                     })}
                 />
             </Button>
