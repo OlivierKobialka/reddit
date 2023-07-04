fb19fd489520c4faeb1630c2f05483b08aeb0042
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-04 14:43:00 +0200

    aaa

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { useCustomToasts } from "@/hooks/use-custom-toasts";
-import { cn } from "@/lib/utils";
+import { PostVoteRequest } from "@/lib/validators/vote";
 import { usePrevious } from "@mantine/hooks";
 import { VoteType } from "@prisma/client";
-import { ArrowBigDown, ArrowBigUp } from "lucide-react";
-import { FC, useEffect, useState } from "react";
-import { Button } from "../ui/Button";
 import { useMutation } from "@tanstack/react-query";
-import { PostVoteRequest } from "@/lib/validators/vote";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "../../hooks/use-toast";
+import { Button } from "../ui/Button";
+import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface PostVoteClientProps {
     postId: string;
@@ -17,33 +18,69 @@ interface PostVoteClientProps {
     initialVote?: VoteType | null;
 }
 
-const PostVoteClient: FC<PostVoteClientProps> = ({
+const PostVoteClient = ({
     postId,
     initialVotesAmt,
     initialVote,
-}) => {
+}: PostVoteClientProps) => {
     const { loginToast } = useCustomToasts();
     const [votesAmt, setVotesAmt] = useState<number>(initialVotesAmt);
     const [currentVote, setCurrentVote] = useState(initialVote);
     const prevVote = usePrevious(currentVote);
 
+    // ensure sync with server
     useEffect(() => {
         setCurrentVote(initialVote);
-    }, [currentVote]);
+    }, [initialVote]);
 
     const { mutate: vote } = useMutation({
-        mutationFn: async (voteType: VoteType) => {
+        mutationFn: async (type: VoteType) => {
             const payload: PostVoteRequest = {
-                postId,
-                voteType,
+                voteType: type,
+                postId: postId,
             };
 
             await axios.patch("/api/subreddit/post/vote", payload);
         },
+        onError: (err, voteType) => {
+            if (voteType === "UP") setVotesAmt((prev) => prev - 1);
+            else setVotesAmt((prev) => prev + 1);
+
+            // reset current vote
+            setCurrentVote(prevVote);
+
+            if (err instanceof AxiosError) {
+                if (err.response?.status === 401) {
+                    return loginToast();
+                }
+            }
+
+            return toast({
+                title: "Something went wrong.",
+                description: "Your vote was not registered. Please try again.",
+                variant: "destructive",
+            });
+        },
+        onMutate: (type: VoteType) => {
+            if (currentVote === type) {
+                // User is voting the same way again, so remove their vote
+                setCurrentVote(undefined);
+                if (type === "UP") setVotesAmt((prev) => prev - 1);
+                else if (type === "DOWN") setVotesAmt((prev) => prev + 1);
+            } else {
+                // User is voting in the opposite direction, so subtract 2
+                setCurrentVote(type);
+                if (type === "UP")
+                    setVotesAmt((prev) => prev + (currentVote ? 2 : 1));
+                else if (type === "DOWN")
+                    setVotesAmt((prev) => prev - (currentVote ? 2 : 1));
+            }
+        },
     });
 
     return (
-        <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
+        <div className="flex flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
+            {/* upvote */}
             <Button
                 onClick={() => vote("UP")}
                 size="sm"
@@ -58,12 +95,18 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
                 />
             </Button>
 
+            {/* score */}
             <p className="text-center py-2 font-medium text-sm text-zinc-900">
                 {votesAmt}
             </p>
+
+            {/* downvote */}
             <Button
                 onClick={() => vote("DOWN")}
                 size="sm"
+                className={cn({
+                    "text-emerald-500": currentVote === "DOWN",
+                })}
                 variant="ghost"
                 aria-label="downvote"
             >
