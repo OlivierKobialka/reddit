4f1335f5b033424eff32402ee8191ee5d59ccf60
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-05 13:14:10 +0200

    fix

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -60,6 +60,22 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
                         }}
                     />
                 </Suspense>
+
+                <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+                    <p className="max-h-40 truncate mt-1 text-xs text-gray-500">
+                        Posted by u/
+                        {post?.author.username ??
+                            cachedPost.authorUsername}{" "}
+                        {formatTimeToNow(
+                            new Date(post?.createdAt ?? cachedPost.createdAt)
+                        )}
+                    </p>
+                    <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+                        {post?.title ?? cachedPost.title}
+                    </h1>
+
+                    <EditorOutput content={post?.content ?? cachedPost.content} />
+                </div>
             </div>
         </div>
     );
--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,6 +1,9 @@
+"use client";
+
+import CustomCodeRenderer from "@/components/renderers/CustomCodeRenderer";
+import CustomImageRenderer from "@/components/renderers/CustomImageRenderer";
+import { FC } from "react";
 import dynamic from "next/dynamic";
-import Image from "next/image";
-import React, { FC } from "react";
 
 const Output = dynamic(
     async () => (await import("editorjs-react-renderer")).default,
@@ -11,6 +14,11 @@ interface EditorOutputProps {
     content: any;
 }
 
+const renderers = {
+    image: CustomImageRenderer,
+    code: CustomCodeRenderer,
+};
+
 const style = {
     paragraph: {
         fontSize: "0.875rem",
@@ -18,39 +26,16 @@ const style = {
     },
 };
 
-const renderers = {
-    image: CustomImageRenderer,
-    // code: CustomCodeRenderer,
-};
-
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
     return (
         // @ts-expect-error
         <Output
-            data={content}
             style={style}
             className="text-sm"
             renderers={renderers}
+            data={content}
         />
     );
 };
-
-function CustomCodeRenderer({ data }: any) {
-    return (
-        <pre className="bg-gray-800 rounded-md p-4">
-            <code className="text-gray-100 text-sm">{data.code}</code>
-        </pre>
-    );
-}
-
-function CustomImageRenderer({ data }: any) {
-    const src = data.file.url;
-
-    return (
-        <div className="relative w-full min-h-[15rem]">
-            <Image alt="image" className="object-contain" fill src={src} />
-        </div>
-    );
-}
 
 export default EditorOutput;
