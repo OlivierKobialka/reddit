bd907e1cb9ab20a2ea97ff9ac729aaa51c96c8d3
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-07-05 12:57:36 +0200

    das

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -50,4 +50,19 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
     );
 };
 
+function PostVoteShell() {
+    return (
+        <div className="flex items-center flex-col pr-6 w-20">
+            {/* upvote */}
+            <div className={buttonVariants({ variant: "ghost" })}>
+                <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+            </div>
+            {/* score */}
+            <div className="text-center py-2 font-medium text-sm text-zinc-900">
+                <Loader2 className="animate-spin h-3 w-3" />
+            </div>
+        </div>
+    );
+}
+
 export default SubRedditPostPage;
